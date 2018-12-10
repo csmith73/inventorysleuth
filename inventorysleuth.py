66db82eb73f03e6b796c08cadb4ac5383259a213
csmith73 <cartersmith73@gmail.com>
2018-12-10 11:32:44 -0500

    Included html theme and coming soon theme

--- inventorysleuth.py
+++ inventorysleuth.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, jsonify, session, abort, flash
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return render_template('comingsoon.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run()
+    #app.run(host='0.0.0.0')
